3dfaa953184f690defbf1f488a22582308fb4722
kapy99 <kapibara2381@gmail.com>
2023-10-29 17:26:48 +0900

    fixed resources path

--- Frontend/vite/src/_entries/main.ts
+++ Frontend/vite/src/_entries/main.ts
@@ -1,6 +1,6 @@
 import "./style.scss";
-import typescriptLogo from "/typescript.svg";
-import viteLogo from "/vite.svg";
+import typescriptLogo from "#/typescript.svg";
+import viteLogo from "~/vite.svg";
 import { setupCounter } from "@/counter";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
