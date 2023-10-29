751a6b625189163f7ebe87d73cfaf773e653e289
kapy99 <kapibara2381@gmail.com>
2023-10-29 18:26:07 +0900

    fixed lint and prettier format rules

---
+++ Frontend/vite/.prettierrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+	// https://prettier.io/docs/en/options
+	printWidth: 120,
+	tabWidth: 2,
+	useTabs: true,
+	singleQuote: true,
+	semi: true,
+	trailingComma: "all", // 末尾のカンマ
+	bracketSpacing: true, // {}　のなかのスペーシング
+	bracketSameLine: true, // 属性値を立て揃えにする
+	arrowParens: "avoid", // 変数一つのアロー関数は、（）を省略する
+	endOfLine: "auto", // lf|crlf|cr|auto
+	embeddedLanguageFormatting: "off", // 埋め込みコードの書式変更を行うかどうか
+
+	// parser: "typescript",
+};
+
