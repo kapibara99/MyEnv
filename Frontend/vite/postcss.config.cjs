0236a091a60763079080ad186340e834c647e41e
kapy99 <kapibara2381@gmail.com>
2023-11-04 22:37:03 +0900

    fixed type lint area

--- Frontend/vite/postcss.config.cjs
+++ Frontend/vite/postcss.config.cjs
@@ -1,14 +1,15 @@
 module.exports = {
-  plugins: {
-    "postcss-normalize-charset": {}, // add charset (アスキー文字がある場合のみに付与される)
-    autoprefixer: {}, // prefix
-    "postcss-sort-media-queries": {}, // media queryを統一にしてくれる
-    "css-declaration-sorter": { order: "concentric-css" }, // props sort smacss or concentric-css , alphabetical // https://www.npmjs.com/package/css-declaration-sorter
-    "@fullhuman/postcss-purgecss": {
-      // 使用していないcss を一切合切、排除する
-      content: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.ts"],
-      // 除外設定 https://purgecss.com/safelisting.html
-      // safelist: ["hoge"],
-    },
-  },
+	plugins: {
+		'postcss-normalize-charset': {}, // add charset (アスキー文字がある場合のみに付与される)
+		autoprefixer: {}, // prefix
+		'postcss-sort-media-queries': {}, // media queryを統一にしてくれる
+		// props sort smacss or concentric-css , alphabetical // https://www.npmjs.com/package/css-declaration-sorter
+		'css-declaration-sorter': { order: 'concentric-css' },
+		'@fullhuman/postcss-purgecss': {
+			// 使用していないcss を一切合切、排除する
+			content: ['./src/**/*.html', './src/**/*.js', './src/**/*.ts'],
+			// 除外設定 https://purgecss.com/safelisting.html
+			// safelist: ["hoge"],
+		},
+	},
 };
--- Frontend/vite/vite.config.modules.ts
+++ Frontend/vite/vite.config.modules.ts
@@ -1,69 +1,64 @@
-import fs from "fs";
-import path from "path";
-export const root = "src";
+import fs from 'node:fs';
+import path from 'path';
+export const root = 'src';
 
 /*
-  この形を自動的に作る
+  このエントリーの形を自動的に作る
   input:{
     index: resolve(__dirname, './index.html'),
     hogehoge_test: resolve(__dirname, './hogehoge/test.html')
   }
 */
 
-type fileConfigType = {
-  name: string;
-  path: string;
-};
+interface fileConfigType {
+	name: string;
+	path: string;
+}
 const files: fileConfigType[] = [];
 function readDirectory(dirPath: string) {
-  const items = fs.readdirSync(dirPath);
+	const items = fs.readdirSync(dirPath);
 
-  for (const item of items) {
-    const itemPath = path.join(dirPath, item);
-    if (fs.statSync(itemPath).isDirectory()) {
-      // componentsディレクトリを除外する
-      if (item === "components") {
-        continue;
-      }
-      readDirectory(itemPath);
-    } else {
-      // htmlファイル以外を除外する
-      if (path.extname(itemPath) !== ".html") {
-        continue;
-      }
+	for (const item of items) {
+		const itemPath = path.join(dirPath, item);
+		if (fs.statSync(itemPath).isDirectory()) {
+			// componentsディレクトリを除外する
+			if (item === 'components') {
+				continue;
+			}
+			readDirectory(itemPath);
+		} else {
+			// htmlファイル以外を除外する
+			if (path.extname(itemPath) !== '.html') {
+				continue;
+			}
 
-      // nameを決定する
-      let targetName = "";
-      if (dirPath === path.resolve(__dirname, root)) {
-        targetName = path.parse(itemPath).name;
-      } else {
-        const relativePath = path.relative(
-          path.resolve(__dirname, root),
-          dirPath
-        );
-        const dirName = relativePath.replace(/\//g, "_");
-        targetName = `${dirName}_${path.parse(itemPath).name}`;
-      }
+			// nameを決定する
+			let targetName = '';
+			if (dirPath === path.resolve(__dirname, root)) {
+				targetName = path.parse(itemPath).name;
+			} else {
+				const relativePath = path.relative(path.resolve(__dirname, root), dirPath);
+				const dirName = relativePath.replace(/\//g, '_');
+				targetName = `${dirName}_${path.parse(itemPath).name}`;
+			}
 
-      // pathを決定する
-      const relativePath = path.relative(
-        path.resolve(__dirname, root),
-        itemPath
-      );
-      const filePath = `/${relativePath}`;
-      // console.log("filePath", path.parse(relativePath).name, filePath);
-      files.push({ name: targetName.replace("pages_", ""), path: filePath });
-    }
-  }
+			// pathを決定する
+			const relativePath = path.relative(path.resolve(__dirname, root), itemPath);
+			const filePath = `/${relativePath}`;
+			// console.log("filePath", path.parse(relativePath).name, filePath);
+			files.push({ name: targetName.replace('pages_', ''), path: filePath });
+		}
+	}
 }
 
 export const exportedInputFiles = () => {
-  readDirectory(path.resolve(__dirname, root));
-  const inputFiles = {};
-  for (let i = 0; i < files.length; i++) {
-    const file = files[i];
-    inputFiles[file.name] = path.resolve(__dirname, "./" + root + file.path);
-  }
-  // console.log("inputFiles", inputFiles);
-  return inputFiles;
+	readDirectory(path.resolve(__dirname, root));
+	const inputFiles: Record<string, string> = {};
+	for (let i = 0; i < files.length; i++) {
+		const file = files[i];
+		if (file === undefined) return;
+		inputFiles[file.name] = path.resolve(__dirname, './' + root + file.path);
+	}
+	// console.log("inputFiles", inputFiles);
+	return inputFiles;
 };
