607169406c2d960bbeff877b32608c6ce32005ea
kapy99 <kapibara2381@gmail.com>
2023-11-19 20:42:37 +0900

    add storybook package

--- Frontend/vite/postcss.config.cjs
+++ Frontend/vite/postcss.config.cjs
@@ -5,11 +5,15 @@ module.exports = {
 		'postcss-sort-media-queries': {}, // media queryを統一にしてくれる
 		// props sort smacss or concentric-css , alphabetical // https://www.npmjs.com/package/css-declaration-sorter
 		'css-declaration-sorter': { order: 'concentric-css' },
-		'@fullhuman/postcss-purgecss': {
-			// 使用していないcss を一切合切、排除する
-			content: ['./src/**/*.html', './src/**/*.js', './src/**/*.ts'],
-			// 除外設定 https://purgecss.com/safelisting.html
-			// safelist: ["hoge"],
-		},
+		// '@fullhuman/postcss-purgecss': {// storybookと相性悪くなるのでコメントアウト
+		// 	// 使用していないcss を一切合切、排除する
+		// 	// content: [
+		// 	// 	// {
+		// 	// 	// 	pattern: './src/**/*[!stories].ts',
+		// 	// 	// },
+		// 	// ],
+		// 	// 除外設定 https://purgecss.com/safelisting.html
+		// 	// safelist: ["hoge"],
+		// },
 	},
 };
