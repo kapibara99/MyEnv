d5f8b25008a64dee444411067e3f195d968e1024
kapy99 <kapibara2381@gmail.com>
2023-12-07 00:34:45 +0900

    fixed fin circle in value case

--- Frontend/vite/src/_modules/components/organisms/chart/chart.shared.ts
+++ Frontend/vite/src/_modules/components/organisms/chart/chart.shared.ts
@@ -2,23 +2,27 @@ import {
 	Chart as ChartJS,
 	// ChartType,
 	// Plugin,
+	ArcElement,
 	BarController,
 	BarElement,
 	CategoryScale,
+	DoughnutController,
+	LineElement,
 	LinearScale,
 	PointElement,
-	LineElement,
 	Title,
 	Tooltip,
 	Legend,
 } from 'chart.js';
 ChartJS.register(
-	CategoryScale,
-	LinearScale,
+	ArcElement,
 	BarController,
 	BarElement,
-	PointElement,
+	CategoryScale,
+	DoughnutController,
 	LineElement,
+	LinearScale,
+	PointElement,
 	Title,
 	Tooltip,
 	Legend,
@@ -26,9 +30,22 @@ ChartJS.register(
 
 // export type ChartType = 'line' | 'vertical-bar' | 'horizontal-bar' | 'circle' | 'circle-in-value';
 
+// chartを初期化する
 export function initializeChart(target: CanvasRenderingContext2D | HTMLCanvasElement | null, options: any) {
 	if (!target) return;
 	let chart = new ChartJS(target, options);
 	return chart;
 }
 
+// 円形チャートなど、正方形の際に幅が飛び出る場合は親最大幅で正方形を保つ
+export function settingDoughnutSize(canvasWrapperEl: HTMLElement | null) {
+	if (!canvasWrapperEl) return;
+	const { offsetWidth } = canvasWrapperEl;
+	const parentWidth = canvasWrapperEl.parentElement!.offsetWidth ?? offsetWidth; // wrapperの外側のwidth
+	// console.log(parentWidth, offsetWidth);
+
+	if (parentWidth < offsetWidth) {
+		canvasWrapperEl.style.width = `${parentWidth}px`;
+		canvasWrapperEl.style.height = `${parentWidth}px`;
+	}
+}
