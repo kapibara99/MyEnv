b44eab18b28def089f44a2a14d19b067f4cab9e4
kapy99 <kapibara2381@gmail.com>
2023-12-13 21:04:51 +0900

    add 後回し atoms memo

--- Frontend/vite/src/_modules/components/atoms/form/form-parts.stories.ts
+++ Frontend/vite/src/_modules/components/atoms/form/form-parts.stories.ts
@@ -1,9 +1,10 @@
 import modules from './form-parts.html?raw'; // rawをパラメータで付与することで、HTMLファイルを静的文字列として取得できる
+import './form-parts.scss';
 
 export default {
 	title: 'Components/Atoms',
 };
 
-export const BasicFormParts = () => {
+export const FormParts = () => {
 	return modules;
 };
