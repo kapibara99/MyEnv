41351041bb97eba834419983d830a315fb70da00
kapy99 <kapibara2381@gmail.com>
2024-09-06 21:46:04 +0900

    style build を仮設定

--- Frontend/astro/astro.config.mjs
+++ Frontend/astro/astro.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,4 +8,27 @@ export default defineConfig({
 			experimentalReactChildren: true,
 		}),
 	],
-})
+	build: {
+		inlineStylesheets: 'never', // https://docs.astro.build/ja/reference/configuration-reference/#buildinlinestylesheets
+	},
+	vite: {
+		build: {
+			rollupOptions: {
+				output: {
+					// 細かいディレクトリ指定をしたいなら、下記を参照
+					// https://cumak.net/blog/astro-build-css/
+					assetFileNames: assetInfo => {
+						let extType = assetInfo.name.split('.')[1];
+						if (/ttf|otf|eot|woff|woff2/i.test(extType)) {
+							extType = 'fonts';
+						}
+						if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+							extType = 'images';
+						}
+						return `_assets/${extType}/[name][extname]`;
+					},
+				},
+			},
+		},
+	},
+});
