18d0c94c82d3b19268369d67d0308422daa1690e
kapy99 <kapibara2381@gmail.com>
2025-02-06 12:42:41 +0900

    multi execute basic実装

--- Backend/python/db_test/tests/db_test.py
+++ Backend/python/db_test/tests/db_test.py
@@ -1,11 +1,21 @@
-from main.db import sql_execute
+from main.db import sql_execute, multi_sql_execute
 
 
 def test_sql_execute() -> None:
   cur = sql_execute("select * from learning.test;")
-  print(cur)
+  assert len(cur) == 5
+
+
+def multi_process_sample(data_id: int) -> None:
+  return sql_execute(f"select * from learning.test where id = {data_id};", dict_cur=True)
+
+
+def test_multi_sql_execute() -> None:
+  id_ary = [1, 2, 3, 4, 5]
+  cur = multi_sql_execute(id_ary, multi_process_sample)
   assert len(cur) == 5
 
 
 if __name__ == "__main__":
   test_sql_execute()
+  test_multi_sql_execute()
