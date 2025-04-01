8336ec376179cb12b90f0c4b329e3a474f68ef72
kapy99 <kapibara2381@gmail.com>
2025-04-01 20:16:25 +0900

    add sample routing page

--- Frontend/nextjs/next.config.ts
+++ Frontend/nextjs/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  trailingSlash: true,
+  output: "export",
 };
 
 export default nextConfig;
