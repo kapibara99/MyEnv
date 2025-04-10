7eb8df114d3c0488ccb4a16a50557774f543c4a6
kapy99 <kapibara2381@gmail.com>
2025-04-10 21:25:11 +0900

    add monorepo projects

---
+++ monorepo/packages/share-types/src/models.ts
@@ -0,0 +1,22 @@
+// packages/shared-types/src/index.ts
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+export type Role = "admin" | "user" | "guest";
+
+export enum Status {
+  Active = "ACTIVE",
+  Inactive = "INACTIVE",
+}
+
+// APIレスポンスの共通型なども定義できる
+export interface ApiResponse<T> {
+  success: boolean;
+  data: T | null;
+  message?: string;
+}
+
+console.log("test");
