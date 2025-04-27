f8f3fd3193dc30662791a0233ff56e6cf31bc954
kapy99 <kapibara2381@gmail.com>
2025-04-27 15:36:25 +0900

    convert vite stories to shared-ui

--- packages/frontend/vite/src/_modules/components/atoms/table/table.stories.ts
+++
@@ -1,9 +0,0 @@
-import modules from './table.html?raw'; // rawをパラメータで付与することで、HTMLファイルを静的文字列として取得できる
-
-export default {
-	title: 'Components/Atoms',
-};
-
-export const BasicTable = () => {
-	return modules;
-};
